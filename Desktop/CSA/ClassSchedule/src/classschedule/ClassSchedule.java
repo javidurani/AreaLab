3df147e80415d8db57d4dd8988d87413714dea38
Javier Duran Iglesias <21122@columbushs.com>
2019-08-30 09:40:53 -0400

    first commit

---
+++ Desktop/CSA/ClassSchedule/src/classschedule/ClassSchedule.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classschedule;
+
+/**
+ *
+ * @author 21122
+ */
+public class ClassSchedule {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String firstName = "Javier";
+        String lastName = "Duran";
+        int year = 2021;
+        System.out.println("My name is " + firstName + " " + lastName + " and I'll graduate in " + year);
+        
+        
+    }
+    
+}
